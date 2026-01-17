364c80aaf3bba7e3cfc2e60d1aff7fda2664f532
Ajay kumar <ajaykumar665748@gmail.com>
2026-01-17 23:50:47 +0530

    Time: 260 ms (22.62%) | Memory: 121.6 MB (17.86%) - LeetSync

---
+++ 1722-throne-inheritance/throne-inheritance.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class ThroneInheritance {
+
+    private String king;
+    private Map<String, List<String>> tree;
+    private Set<String> dead;
+
+    public ThroneInheritance(String kingName) {
+        king = kingName;
+        tree = new HashMap<>();
+        dead = new HashSet<>();
+        tree.put(king, new ArrayList<>());
+    }
+
+    public void birth(String parentName, String childName) {
+        tree.get(parentName).add(childName);
+        tree.put(childName, new ArrayList<>());
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        List<String> result = new ArrayList<>();
+        dfs(king, result);
+        return result;
+    }
+
+    private void dfs(String person, List<String> result) {
+        if (!dead.contains(person)) {
+            result.add(person);
+        }
+        for (String child : tree.get(person)) {
+            dfs(child, result);
+        }
+    }
+}
