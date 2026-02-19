5fd81db25533eb05709d10aa37b082cdc54a61d1
Ajay kumar <ajaykumar665748@gmail.com>
2026-02-19 23:26:33 +0530

    Time: 15 ms (6.91%) | Memory: 47.9 MB (70.37%) - LeetSync

--- 4156-minimum-cost-to-make-two-binary-strings-equal/minimum-cost-to-make-two-binary-strings-equal.java
+++ 4156-minimum-cost-to-make-two-binary-strings-equal/minimum-cost-to-make-two-binary-strings-equal.java
@@ -9,24 +9,14 @@ class Solution {
             else if (s.charAt(i) == '1' && t.charAt(i) == '0') cnt10++;
         }
 
-        // We use DP to find the minimum cost to reach the state (cnt01, cnt10)
-        // Since we can always resolve a (0,1) and (1,0) together, or two of the same,
-        // we can simplify the state to the number of mismatches of each type.
         
-        // However, a simple greedy approach with a slight adjustment works:
-        // You can convert a (0,1) to (1,0) for 'crossCost'.
-        // So we can try all possible number of cross-swaps.
         
         long minTotal = Long.MAX_VALUE;
         
-        // Let i be the number of (0,1) we convert to (1,0)
-        // OR let i be the number of (1,0) we convert to (0,1)
-        // Since we want to balance them to use swapCost, we only need to check
-        // a small range around the balance point.
+        
         
         for (int i = -cnt01; i <= cnt10; i++) {
-            // i > 0 means we convert i (1,0)s into (0,1)s
-            // i < 0 means we convert |i| (0,1)s into (1,0)s
+    
             long new01 = cnt01 + i;
             long new10 = cnt10 - i;
             
