3d2df3e2d9dc9765666c6d16f6edeac096bf15b5
Ajay kumar <ajaykumar665748@gmail.com>
2026-05-06 22:31:46 +0530

    Time: 18 ms (80.60%) | Memory: 50.9 MB (36.59%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -2,18 +2,21 @@ import java.util.*;
 
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0) return 0;
-
+        int rows = matrix.length;
         int cols = matrix[0].length;
+
         int[] heights = new int[cols];
         int maxArea = 0;
 
-        for (char[] row : matrix) {
-           
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                heights[j] = (row[j] == '1') ? heights[j] + 1 : 0;
+                if (matrix[i][j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
             }
-           
+
             maxArea = Math.max(maxArea, largestRectangleArea(heights));
         }
 
@@ -28,10 +31,18 @@ class Solution {
             int currHeight = (i == heights.length) ? 0 : heights[i];
 
             while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
-                int h = heights[stack.pop()];
-                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, h * width);
+                int height = heights[stack.pop()];
+                int width;
+
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                maxArea = Math.max(maxArea, height * width);
             }
+
             stack.push(i);
         }
 
