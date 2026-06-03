bc0d34b53db06bd19b13c5a0c50c69c4e54b2d6c
Ajay kumar <ajaykumar665748@gmail.com>
2026-06-03 13:49:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (46.19%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,13 +2,9 @@ class Solution {
     public int uniquePaths(int m, int n) {
 
         int[][] dp = new int[m][n];
-
-        // First row
         for (int j = 0; j < n; j++) {
             dp[0][j] = 1;
         }
-
-        // First column
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
         }
