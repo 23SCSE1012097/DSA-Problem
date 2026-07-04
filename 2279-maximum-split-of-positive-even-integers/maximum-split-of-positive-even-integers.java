54149f77ef1b91881539d9fa11a134e8010b41bd
Ajay kumar <ajaykumar665748@gmail.com>
2026-07-04 22:18:20 +0530

    Time: 9 ms (99.40%) | Memory: 111.7 MB (47.41%) - LeetSync

---
+++ 2279-maximum-split-of-positive-even-integers/maximum-split-of-positive-even-integers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Long> maximumEvenSplit(long finalSum) {
+        List<Long> ans = new ArrayList<>();
+
+        if ((finalSum & 1) == 1) {
+            return ans;
+        }
+
+        long curr = 2;
+
+        while (finalSum >= curr) {
+            ans.add(curr);
+            finalSum -= curr;
+            curr += 2;
+        }
+
+        if (finalSum > 0) {
+            int last = ans.size() - 1;
+            ans.set(last, ans.get(last) + finalSum);
+        }
+
+        return ans;
+    }
+}
