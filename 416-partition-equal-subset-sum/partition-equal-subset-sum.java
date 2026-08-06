eb3e724a076ba1eecdaa1d1d64c658fe2e039929
Ajay kumar <ajaykumar665748@gmail.com>
2026-08-06 13:23:01 +0530

    Time: 120 ms (6.53%) | Memory: 72.4 MB (35.50%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,32 +1,49 @@
 class Solution {
-    private boolean subset (int i , int [] arr, int target, int [][] dp){
-        if (i==arr.length){
-            if (target==0) return true ;
-            else return false ;
-        }
-        if (dp[i] [target] !=-1) return (dp[i] [target]==1);
-        boolean ans = false;
-        boolean skip = subset(i+1, arr, target, dp);
-        if (target-arr[i]<0) ans = skip;
-        else {
-            boolean pick = subset (i+1,arr, target-arr[i], dp);
-            ans = pick || skip;
 
+    public boolean subset(int i, int[] arr, int target, int[][] dp) {
+
+        if (target == 0)
+            return true;
+
+        if (i == arr.length)
+            return false;
+
+        if (dp[i][target] != -1)
+            return dp[i][target] == 1;
+
+        boolean skip = subset(i + 1, arr, target, dp);
+
+        boolean pick = false;
+        if (arr[i] <= target) {
+            pick = subset(i + 1, arr, target - arr[i], dp);
         }
-        dp[i][target] = (ans) ? 1:0;
+
+        boolean ans = pick || skip;
+
+        dp[i][target] = ans ? 1 : 0;
+
         return ans;
     }
 
     public boolean canPartition(int[] nums) {
-        int sum =0;
-        for (int ele:nums) sum+= ele;
-        if (sum%2!=0) return false;
-        int target = sum/2;
-        int [][] dp = new int [nums.length] [target+1];
-        for(int i=0; i<dp.length; i++)
-        for (int j=0; j<dp[0].length; j++) dp[i][j] =-1;
+
+        int sum = 0;
+
+        for (int x : nums)
+            sum += x;
+
+        if (sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+        int n = nums.length;
+
+        int[][] dp = new int[n][target + 1];
+
+        for (int i = 0; i < n; i++) {
+            java.util.Arrays.fill(dp[i], -1);
+        }
+
         return subset(0, nums, target, dp);
-         
-        
     }
 }
