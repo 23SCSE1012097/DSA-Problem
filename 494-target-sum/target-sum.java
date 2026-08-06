c522fac2a4f7d970172413a2e02001c94d200846
Ajay kumar <ajaykumar665748@gmail.com>
2026-08-06 11:13:14 +0530

    Time: 9 ms (55.88%) | Memory: 47 MB (18.64%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,25 +1,41 @@
 class Solution {
+
+    int sum;
+
+    public int ways(int i, int[] arr, int res, int target, int[][] dp) {
+
+        if (i == arr.length) {
+            return (res == target) ? 1 : 0;
+        }
+
+        if (dp[i][res + sum] != -1) {
+            return dp[i][res + sum];
+        }
+
+        int add = ways(i + 1, arr, res + arr[i], target, dp);
+        int sub = ways(i + 1, arr, res - arr[i], target, dp);
+
+        return dp[i][res + sum] = add + sub;
+    }
+
     public int findTargetSumWays(int[] nums, int target) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+
+        sum = 0;
+
+        for (int x : nums) {
+            sum += x;
         }
 
-        if (Math.abs(target) > totalSum || (target + totalSum) % 2 != 0) {
+        if (Math.abs(target) > sum)
             return 0;
+
+        int[][] dp = new int[nums.length][2 * sum + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            java.util.Arrays.fill(dp[i], -1);
         }
 
-        int sum = (target + totalSum) / 2;
-
-        int[] dp = new int[sum + 1];
-        dp[0] = 1;
-
-        for (int num : nums) {
-            for (int s = sum; s >= num; s--) {
-                dp[s] += dp[s - num];
-            }
-        }
-
-        return dp[sum];
+        return ways(0, nums, 0, target, dp);
     }
 }
+        
