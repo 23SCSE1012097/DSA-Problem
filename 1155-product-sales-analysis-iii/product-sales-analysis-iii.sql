1a05d1f1791c43c9b07302057cf01cabbb64970f
Ajay kumar <ajaykumar665748@gmail.com>
2026-09-21 23:53:03 +0530

    Time: 4269 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,12 +1,13 @@
 # Write your MySQL query statement below
-SELECT
-     product_id, 
-     year AS first_year,
-     quantity,
-     price
-FROM sales
-WHERE (product_id, year) IN (
-    SELECT product_id, MIN(year) AS f_year
-    FROM sales
+SELECT s.product_id,
+       s.year AS first_year,
+       s.quantity,
+       s.price
+FROM Sales s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
     GROUP BY product_id
-)
+) f
+ON s.product_id = f.product_id
+AND s.year = f.first_year;
